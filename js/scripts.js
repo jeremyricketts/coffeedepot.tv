b88d9633bc538f18d73a30ca929789add1db2833
Jeremy Ricketts <email@jeremyricketts.com>
2013-05-24 15:23:30 -0700

    added files

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// <![CDATA[
+$(document).ready(function() {
+	
+$('.location-pic-thumb').facebox();
+
+});
+
+
+
+
+
+//=========== MAPS =============
+$(window).load(function () {
+	
+	// $('#front-slideshow').css('visibility', 'visible');
+	
+	// //initiate the front page slideshow
+	// $('#front-slideshow').cycle({ 
+	//     fx: 'fade', 
+	//     speed: 600,
+	// 	timeout: 9000,
+	// 	next: '#slideshowNext',
+	//     prev: '#slideshowPrev'
+	//  });
+	
+});
+
+// ]]>
